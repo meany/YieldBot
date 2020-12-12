483c3e42d034d1ce63bdf783c06ed15ca179daa3
meany <dmeany@gmail.com>
2020-12-12 11:13:21 -0500

    telegram basics

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace dm.YLD.TelegramBot
 {
@@ -81,36 +82,8 @@ namespace dm.YLD.TelegramBot
                 }
                 else
                 {
-                    return;
-
-                    var item = await Data.Common.GetStats(db);
-
-                    string text = $"" +
-                        $"🤑 Price/USD: ${item.Price.PriceUSD.FormatUsd()}\n" +
-                        $"🤑 Price/BTC: ₿{item.Price.PriceBTC.FormatBtc()}\n" +
-                        $"🤑 Price/ETH: Ξ{item.Price.PriceETH.FormatEth()}\n" +
-                        $"📈 Market Cap: ${item.Price.MarketCapUSD.FormatLarge()}\n" +
-                        $"💸 Volume: ${item.Price.VolumeUSD.FormatLarge()}";
-
-                    foreach (long chatId in config.ChatIds)
-                    {
-                        await botClient.SendTextMessageAsync(
-                          chatId: chatId,
-                          text: text
-                        );
-
-                        log.Info($"Stats sent to {chatId}");
-                    }
-
-                    if (item.IsOutOfSync())
-                    {
-                        await botClient.SendTextMessageAsync(
-                            chatId: config.AdminId,
-                            text: "Stats out of sync."
-                        );
-
-                        log.Info("Price out of sync.");
-                    }
+                    log.Info("BotWatch = false, sending ad-hoc message");
+                    return;                   
                 }
             }
             catch (Exception ex)
@@ -121,12 +94,55 @@ namespace dm.YLD.TelegramBot
 
         private async Task RunBotArgs(string[] args, ITelegramBotClient botClient)
         {
+            log.Info("Running with args");
             return;
         }
 
-        private void BotClient_OnMessage(object sender, MessageEventArgs e)
+        private async void BotClient_OnMessage(object sender, MessageEventArgs e)
         {
-            log.Info($"ChatId: {e.Message.Chat.Id}, Message: {e.Message.Text}");
+            if (e.Message.Text == null || !e.Message.Text.StartsWith('/'))
+                return;
+
+            string cmd = e.Message.Text.Substring(1);
+            log.Info($"ChatId: {e.Message.Chat.Id}, Command: {cmd}");
+
+            string reply = await GetCmdReply(cmd);
+            if (string.IsNullOrEmpty(reply))
+                return;
+
+            await botClient.SendTextMessageAsync(
+              chatId: e.Message.Chat,
+              text: reply,
+              parseMode: ParseMode.Html,
+              disableNotification: true
+            );
+        }
+
+        private async Task<string> GetCmdReply(string cmd)
+        {
+            string reply = string.Empty;
+
+            switch (cmd)
+            {
+                case "price":
+                    var price = await Data.Common.GetPrices(db);
+                    reply = $"$ <b>{price.PriceUSD.FormatUsd()}</b>\n" +
+                        $"₿ <b>{price.PriceBTC.FormatBtc()}</b>\n" +
+                        $"Ξ <b>{price.PriceETH.FormatEth()}</b>";
+                    break;
+                case "supply":
+                    var supply = await Data.Common.GetStats(db);
+                    reply = $"Supply: <b>{supply.Stat.Supply.FormatYld()}</b> $YLD\n" +
+                        $"Circulation: <b>{supply.Stat.Circulation.FormatYld()}</b> $YLD";
+                    break;
+                case "mcap":
+                    var mcap = await Data.Common.GetPrices(db);
+                    reply = $"Market Cap: $ <b>{mcap.MarketCapUSD.FormatLarge()}</b>\n" +
+                        $"Volume (24h): $ <b>{mcap.VolumeUSD.FormatLarge()}</b>";
+                    break;
+            }
+
+            return reply;
         }
     }
 }
